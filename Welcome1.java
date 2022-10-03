d31170d6055505f4b4fbe43a3fb48b4de9593050
DReilly137 <G00420230@atu.ie>
2022-10-03 19:08:40 +0000

    commit

---
+++ Welcome1.java
@@ -0,0 +1,13 @@
+// Welcome.java
+// Text-printing program
+
+public class Welcome1{
+
+    //main method begins the execution of java Application
+    public static void main(String[] args){
+        
+        System.out.println("Welcome to Java Programming!");
+
+    }//end of main method
+
+}//end of class
