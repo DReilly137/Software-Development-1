c8ee4cdb75469829b89d44c25c1f1836049ce74f
DReilly137 <G00420230@atu.ie>
2022-10-11 11:49:04 +0000

    commit

---
+++ 2/StringConCat.java
@@ -0,0 +1,11 @@
+public class StringConCat {
+    public static void main(String[] args){
+        int age= 20;
+
+        System.out.println("Age: " + age);
+
+        System.out.println(age + "years old");
+
+        System.out.println("Age is " + age + "years old");
+    }
+}
