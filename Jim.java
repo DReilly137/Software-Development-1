9cd948fd21c2b0b813c86d5b1e68a244dc64d361
DReilly137 <G00420230@atu.ie>
2022-10-17 11:53:31 +0000

    commit

---
+++ Jim.java
@@ -0,0 +1,18 @@
+public class Jim{
+
+    public static void main(String[] args){
+
+        String name= "Jim";
+        int age= 40;
+        String job= "Plumber";
+        int children= 8;
+        int earns= 700;
+        double exam= 27.5;
+        int needs= 800;
+
+        System.out.println(name + " is a " + age + "-year-old man. Life is tough on " + name + ". He works as a " + job + ". He is divorced and has " +
+                          children + " children. He earns " + earns + " a week but needs " + needs + " a week to support his ex-wife and children. " +
+                           "He was hoping to pass a exam which would enable him to earn more, but unfortunately poor " + name + " failed it " +
+                           "with a score of " + exam + "." );
+    }
+}
