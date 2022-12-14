91801728415fcfe4a77836d7ffe042ae45a44af3
DReilly137 <G00420230@atu.ie>
2022-12-14 12:47:52 +0000

    commit

---
+++ W.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class W{
+
+    public static void main(String[] args){
+
+        Scanner input = new Scanner (System.in);
+
+        System.out.println("***         ****         ***");
+        System.out.println(" ***       ******       ***");
+        System.out.println("  ***     ***  ***    ***");
+        System.out.println("   ***   ***    ***  ***");
+        System.out.println("    ********     ******");
+        System.out.println("      ****        ****");
+        System.out.println();
+        System.out.println();
+        
+    }
+}
+
+// Darren Reilly 13/12/2022
