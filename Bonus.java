941b1176275ae02c7dc2ac02dd66f3a426583ed0
DReilly137 <G00420230@atu.ie>
2022-12-20 10:49:03 +0000

    commit

--- Bonus.java
+++ Bonus.java
@@ -5,17 +5,24 @@ public class Bonus{
 
         Scanner input = new Scanner (System.in);
         double wage;
-        char grade;
 
         System.out.print("Enter weekly wage: ");
         wage = input.nextDouble();
         System.out.print("Enter grade: ");
-        grade = input.nextChar();
+        char grade = input.next().charAt(0);
 
         switch(grade){
             case 'A':
                 System.out.println("You get an extra 100 euro bonus!");
                 System.out.println("Your new wage is "+(wage+100));
+                break;
+            case 'B':
+                System.out.println("You get an extra 50 euro bonus!");
+                System.out.println("Your new wage is "+(wage+50));
+                break;
+            case 'C':
+                System.out.println("You get an extra 10 euro bonus!");
+                System.out.println("Your new wage is "+(wage+10));
                 break;
         }
 
