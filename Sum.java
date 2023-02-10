edff78be478230fcbf5185e1d5daef62ed1d4427
DReilly137 <G00420230@atu.ie>
2023-02-10 08:44:50 +0000

    commit

--- Sum.java
+++ Sum.java
@@ -4,13 +4,19 @@ public class Sum{
     public static void main(String[] args){
 
         Scanner input = new Scanner (System.in);
-        int i=0, sum=0;
+        int i1=0, i2=2, sum1=0, sum2=0;
 
-        while(i<100){
-            sum+=i;
-            ++i;
+        while(i1<100){
+            sum1+=i1;
+            ++i1;
         }
-        System.out.println("Sum of numbers from 1 to 100: "+sum);
+        System.out.println("Sum of numbers from 1 to 100: "+sum1);
+
+        while(i2<=100){   
+            sum2+=i2; 
+            i2+=2; 
+        }     
+        System.out.println(sum2); 
 
     }
 }
