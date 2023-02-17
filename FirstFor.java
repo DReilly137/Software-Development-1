ef20d640dc10fcdd9925d583f98b4160b247490f
DReilly137 <G00420230@atu.ie>
2023-02-17 09:58:58 +0000

    commit

---
+++ FirstFor.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class FirstFor{
+
+    public static void main(String[] args){
+
+        Scanner input = new Scanner (System.in);
+
+        for(int counter=1; counter<= 20; i++){
+            System.out.println(counter);
+        }
+
+    }
+}
