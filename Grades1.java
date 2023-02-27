e437a577c9df5374913fd2e759df556190d62bde
DReilly137 <G00420230@atu.ie>
2023-02-27 12:49:57 +0000

    commit

---
+++ Grades1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Grades1{
+
+    public static void main(String[] args){
+
+        Scanner input = new Scanner (System.in);
+        int i=0;
+        int grade=0;
+        int sum=0;
+
+        while(i<10){
+            sum += grade;
+            System.out.print("Enter grade: ");
+            grade = input.nextInt();
+            ++i;
+        }
+        System.out.println(((double) sum/i));
+        input.close();
+
+    }
+}
