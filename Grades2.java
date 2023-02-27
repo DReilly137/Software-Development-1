d0b32b184c5033fa8b59da1c12fc0fa8b2a5e4a6
DReilly137 <G00420230@atu.ie>
2023-02-27 18:58:00 +0000

    commit

--- Grades2.java
+++ Grades2.java
@@ -4,6 +4,17 @@ public class Grades2{
     public static void main(String[] args){
 
         Scanner input = new Scanner (System.in);
+        int i=0;
+        int grade=0;
+        int sum=0;
+
+        do{
+            sum += grade;
+            System.out.print("Enter grade "+i+" (0 to quit): ");
+            grade=input.nextInt();
+            ++i;
+        }while(grade != 0);
+            System.out.println("Average grade is "+((double) sum/i));
 
     }
 }
