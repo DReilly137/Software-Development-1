bac4cb23b6b7b00f84563cddc78cd128d2222ab1
DReilly137 <G00420230@atu.ie>
2023-03-02 22:01:11 +0000

    commit

--- Triangle2.java
+++ Triangle2.java
@@ -1,9 +1,6 @@
-import java.util.Scanner;
 public class Triangle2{
 
     public static void main(String[] args){
-    Scanner input = new Scanner (System.in);
-
 
         for(int rows=8; rows>0; rows--){
            for(int columns=0; columns<rows; columns++){
