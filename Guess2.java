e7ab10013ea6ee384072d1f3470facc7dfcaeea1
DReilly137 <G00420230@atu.ie>
2023-03-03 10:45:15 +0000

    commit

--- Guess2.java
+++ Guess2.java
@@ -4,14 +4,29 @@ public class Guess2{
     public static void main(String[] args){
 
         Scanner input = new Scanner (System.in);
-        int number, guess=5, i=0;
+        int number = 5;
+        int i=1, guess;
+        String choice;
 
-        do{
+        for(;;){
             System.out.print("Guess the number: ");
-            number = input.nextInt();
-            i++;
-        }while(number != guess);
-            System.out.println("Good guess. It is "+guess+". It took you "+i+" attempt(s) to guess the number.");
+            guess = input.nextInt();
 
+            if(number == guess){
+                System.out.println("good guess. It is "+number+". It took you "+i+" attempts to guess the number.");
+                System.out.print("play again? (y/n): ");
+                choice = input.next();
+                if(choice.equals("n")){
+                    System.out.println("Game Over");
+                    break;
+                }else if(choice.equals("y")){
+                    number+=2;
+                    i=1;
+                }
+            }else if(number != guess){
+                ++i;
+            }
+        }          
+        
     }
 }
