5cf7a686a31caff791335bd17cf08086ae2677ec
DReilly137 <G00420230@atu.ie>
2023-03-07 12:57:34 +0000

    commit

---
+++ NestedLoop.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class NestedLoop{
+
+    public static void main(String[] args){
+
+        Scanner input = new Scanner (System.in);
+
+        for(row=1; row <= 5; ++row){
+            for(column=1; column <= 3; ++column){
+                
+            }
+        }
+
+    }
+}
