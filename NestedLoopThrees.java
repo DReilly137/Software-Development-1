92d52f24937459564d78332cfcdc9a7368bd9620
DReilly137 <G00420230@atu.ie>
2023-03-08 19:29:38 +0000

    commit

--- NestedLoopThrees.java
+++ NestedLoopThrees.java
@@ -4,8 +4,19 @@ public class NestedLoopThrees{
     public static void main(String[] args){
 
         Scanner input = new Scanner (System.in);
+        int choice, r=1, i=2;
 
-        for(int row=1; row <= 1; row++){
+        System.out.print("Have 1 row, need another? Enter 0 for no: ");
+        choice = input.nextInt();
+
+        while(choice == 1){
+            r+=1;
+            System.out.print("Have "+i+" rows, need another? Enter 0 for no: ");
+            choice = input.nextInt();
+            ++i;
+        }
+
+        for(int row=1; row <= r; row++){
             for(int column=1; column <= 5; column++){
                 if(column <5){
                     System.out.print("3, ");
