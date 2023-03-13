513fe87c22195aefadf9265b8ba6d4c8ed1b87c0
DReilly137 <G00420230@atu.ie>
2023-03-13 23:20:39 +0000

    commit

---
+++ Exercise4.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Exercise4{
+
+    public static void main(String[] args){
+
+        Scanner input = new Scanner (System.in);
+
+        for(int i=1; i <= 20; i++){
+            System.out.println("Week "+i);
+        }
+        input.close();
+        
+    }
+}
---
+++ Exercise5.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Exercise5{
+
+    public static void main(String[] args){
+
+        Scanner input = new Scanner (System.in);
+        int counter = 1;
+
+        for(int i=1; i <= 4; i++){
+            for(int j=1; j <= 4; j++){
+                System.out.print("\t");
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+        
+    }
+}
