a4c80b5874de590e6dab83d6ae74024b4ae0771b
DReilly137 <G00420230@atu.ie>
2023-03-23 20:55:58 +0000

    commit

--- SearchStudent.java
+++ SearchStudent.java
@@ -11,8 +11,26 @@ public class SearchStudent{
             {"Joe Kelly", "Computing and Digital Media", "Year 2", "ATU"}
         };
         System.out.print("Please enter a search value: ");
-        String which = input.nextLine();
-        
+        String find = input.nextLine();
 
+        int matches = 0;
+        boolean found = false;
+        for(int i=0; i < students.length; i++){
+            for(int j=0; j < students[i].length; j++){
+                if(students[i][j].equals(find)){
+                    System.out.println(students[i][0]+"\t"+students[i][3]);
+                    found = true;
+                    matches++;
+                    break;
+                }
+            }
+        }
+        if(!found){
+            System.out.println();
+            System.out.println("Records with match: "+matches);
+        }else{
+            System.out.println();
+            System.out.println("Records with match: "+matches);
+        }
     }
 }
