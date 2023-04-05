0c5e102deaaabc18f2e8d0e008cd3cd112895443
DReilly137 <G00420230@atu.ie>
2023-04-05 14:45:39 +0000

    commit

--- Reverse.java
+++ Reverse.java
@@ -9,7 +9,20 @@ public class Reverse{
         do{
             System.out.print("Enter an integer (-1 to exit): ");
             n = input.nextInt();
+            if(n != -1){
+                System.out.println(ReverseNum(n));
+            }
         }while(n != -1);
 
+    }
+
+    static int ReverseNum(int a){
+        int reversed = 0;
+        while (a != 0){
+            int num = a % 10;
+            reversed = reversed * 10 + num;
+            a /= 10;
+        }
+        return reversed;
     }
 }
