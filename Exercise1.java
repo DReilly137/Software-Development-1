2acde99c900f78e961b32146d33d5c96d1062226
DReilly137 <G00420230@atu.ie>
2023-04-18 11:09:58 +0000

    commit

---
+++ Exercise1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Exercise1{
+
+    public static void main(String[] args){
+
+        Scanner input = new Scanner (System.in);
+        String job = "Carpenter";
+        int age = 47;
+        Double income = 878.23;
+
+        System.out.println("Jim works as a "+job+".");
+        System.out.println("He is "+age+" years old.");
+        System.out.println("He earns "+income+" after tax.");
+
+    }
+}
--- Exercise4.java
+++ Exercise4.java
@@ -4,26 +4,16 @@ public class Exercise4
 {
     public static void main(String[] args)
     {
-        Scanner sc = new Scanner(System.in);
- 
-        //Taking rows value from the user
- 
-        System.out.println("How many rows you want in this pattern?");
- 
-        int rows = sc.nextInt();
- 
-        System.out.println("Here is your pattern....!!!"); 
+        Scanner input = new Scanner(System.in);
+
+        int rows =3;
          
         for (int i = 1; i <= rows; i++)
         {
-            //Printing rows-i spaces at the beginning of each row
- 
             for (int j = 1; j <= rows-i; j++)
             {
                 System.out.print(" ");
             }
- 
-            //Printing i to 2*i value at the end of each row
  
             for (int j = i; j < 2*i; j++)
             {
@@ -33,8 +23,6 @@ public class Exercise4
             System.out.println();
         }
  
-        //Closing the resources
- 
-        sc.close();
+        input.close();
     }
 }
